ecf551cdfb59a9872f05aa254bce1a811b8669ff
FedeLupianez <lupianezfederico@gmail.com>
2025-06-04 11:29:31 -0300

    Add mini.splitjoin to hide large lists

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,9 +3,15 @@ return {
 		"echasnovski/mini.animate",
 		enabled = false,
 	},
-
 	{
 		"echasnovski/mini.nvim",
 		enabled = false,
+	},
+	{
+		"echasnovski/mini.splitjoin",
+		version = false,
+		config = function()
+			require("mini.splitjoin").setup({})
+		end,
 	},
 }
