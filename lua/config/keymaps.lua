fe646728c7badc87a4d7b8a2009f8f6dcc09cff8
FedeLupianez <lupianezfederico@gmail.com>
2025-06-10 19:03:39 -0300

    delete keymaps to snacks.picker and buffer delete

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,32 +5,14 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- Delete a word backwards
-keymap.set("n", "dw", 'vb"_d')
-
--- Find filewith snacks :
-keymap.set("n", "<Leader>ff", "<CMD>lua Snacks.dashboard.pick('files')<CR>", { desc = "Find file" })
-
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
-
--- Delete buffer
-keymap.set("n", "<C-o>", ":bdelete<CR>", { desc = "Delete buffer" })
 
 -- Move text up and down
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
--- Delete char
--- keymap.set("n", "s", "<Del>", { noremap = true })
-
 -- Oil plugin
 keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open Oil" })
 
--- Delete all the buffers except the active
-keymap.set(
-	"n",
-	"<leader>bq",
-	'<Esc>:%bdelete|edit #|normal`"<Return>',
-	{ desc = "Delete other buffers but the current one" }
-)
+keymap.set("n", "4", "$")
