fa1be46516d2bf88f46f437375cbbb93962fc5e6
FedeLupianez <lupianezfederico@gmail.com>
2025-06-10 19:10:21 -0300

    Add support to arduino then cpp, and configuration to toggleterm shell

--- init.lua
+++ init.lua
@@ -2,7 +2,19 @@
 require("config.lazy")
 vim.opt.tabstop = 2 -- Número de espacios que ocupa el tab
 vim.opt.shiftwidth = 2 -- Tamaño de indentación
+vim.opt.guicursor = ""
 
 vim.opt.ttyfast = true -- Asume una terminal rapida
 vim.opt.updatetime = 300 -- Reduce el tiempo de espera para eventos
-vim.diagnostic.config({ virtual_text = false })
+vim.diagnostic.config({ virtual_text = true })
+
+vim.o.shell = "pwsh.exe"
+vim.o.shellcmdflag = "/c"
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.ino",
+	callback = function()
+		vim.bo.filetype = "cpp"
+	end,
+})
